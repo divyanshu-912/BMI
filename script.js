6075bae1781802ef94c9d6ff3c3f27cd034631e3
divyanshu-912 <divyanshukannau143@outlook.com>
2024-11-22 23:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function result() {
+
+ var djk = (document.getElementById(
+  "ashu").value / (document
+  .getElementById("divyanshu")
+  .value *
+  document.getElementById(
+   "divyanshu")
+  .value));
+ var ashuk = document.getElementById(
+  "Equal").value += djk;
+
+
+ if (ashuk <= 18) {
+  alert("Khaya-piya kro thoda = You are Malnutrite ")
+ } else if (ashuk >= 25) {
+  alert(
+   "Kha kha ke mote ho gaye ho.You are Overweight"
+  )
+ } else if (ashuk >= 0) {
+  alert("Your are Healthy")
+ } else {
+  alert("Enter data")
+   }
+}
+
+
+//variables
+let R, G, B;
+let randomR, randomG, randomB;
+
+//function that changes the background color
+function color() {
+ randomR = Math.floor(Math.random() *
+  256);
+ randomG = Math.floor(Math.random() *
+  156);
+ randomB = Math.floor(Math.random() *
+  256);
+ R = randomR;
+ G = randomG;
+ B = randomB;
+ document.body.style
+  .backgroundColor =
+  'rgb' + '(' + R + ',' + G + ',' +
+  B +
+  ')';
+}
